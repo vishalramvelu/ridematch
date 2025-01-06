aa6ecb2c1ec690821057ce73a65ccd32f01321de
vishalramvelu <108497710+vishalramvelu@users.noreply.github.com>
2025-01-06 15:52:12 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xy9TkAcut', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ocoe714cnor86qd3u68e09r7t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_5FUxkqsQu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2fo10mnq508ihnub6fbrhsakri', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
